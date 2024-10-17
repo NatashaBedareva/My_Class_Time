8faf7cfa503d386f4e32bfa9033840b2574062cb
NatashaBedareva <116941649+NatashaBedareva@users.noreply.github.com>
2024-10-18 00:20:39 +0700

    Delete cmake-build-debug directory

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Bedareva/Documents/STADING_NGU/OOP/kvadraturawn/test
-# Build directory: C:/Users/Bedareva/Documents/STADING_NGU/OOP/kvadraturawn/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Bedareva/Documents/STADING_NGU/OOP/kvadraturawn/cmake-build-debug/test/test_time_app[1]_include.cmake")
-subdirs("../_deps/googletest-build")
